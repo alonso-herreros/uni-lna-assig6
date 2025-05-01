aeebb2d94936669a4dca3f6e9df00c23ff2afa95
Alonso Herreros <98601593+alonso-herreros@users.noreply.github.com>
2025-05-01 21:40:44 +0200

    Add permissions and tests (#9)
    Also added a Makefile

---
+++ Makefile
@@ -0,0 +1,41 @@
+TEST_DIR = test
+
+BUILD_DIR = build
+MARKER_DIR = ${BUILD_DIR}/markers
+
+HOST = ldapi:///
+
+all: base permissions
+test: test-permissions
+
+.PHONY: all test clean base permissions passwords test-permissions
+
+BASE_MARKER = ${MARKER_DIR}/base.marker
+base: ${BASE_MARKER}
+${BASE_MARKER}: entries/base.ldif | ${MARKER_DIR}
+	sudo ldapadd -xWD 'cn=admin,dc=marvel,dc=com' -f $?
+	@touch $@
+
+PERMISSIONS_MARKER = ${MARKER_DIR}/permissions.marker
+permissions: ${PERMISSIONS_MARKER}
+${PERMISSIONS_MARKER}: updates/permissions.ldif | ${MARKER_DIR}
+	sudo ldapmodify -WY EXTERNAL -f $?
+	@touch $@
+
+# TODO: make this depend on an actual passwords LDIF
+PASSWORDS_MARKER = ${MARKER_DIR}/passwords.marker
+passwords: ${PASSWORDS_MARKER}
+${PASSWORDS_MARKER}: ${TEST_DIR}/passwords/* | ${MARKER_DIR}
+	test/set-passwords.sh -p "${TEST_DIR}/passwords"
+	@touch $@
+
+
+test-permissions: base permissions passwords
+	test/test-permissions.sh -p "${TEST_DIR}/passwords"
+
+${MARKER_DIR}:
+	mkdir -p $@
+
+clean:
+	sudo ldapmodify -xWD 'cn=admin,dc=marvel,dc=com' -f updates/icoe-nuke.ldif
+	rm -rf ${BUILD_DIR}
---
+++ test/lib/ldap_test_utils.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+# vim: ts=4
+
+# ---- Overridable defaults ----
+[ -z "$PASSWORD_DIR" ] && PASSWORD_DIR="passwords"
+
+# Abstraction to test an arbitrary access level
+function _test_ldap_access() {
+	# First arg should be access level
+	level="$1"
+	shift
+
+	case "$level" in "?" | "??" )
+		return 0;;
+	esac
+
+	echo "Test '$level' access by '$as' to '$to': '$attr'"
+
+	case "$level" in
+		W | RW )
+			_test_ldap_write "$@";;
+		R | R? )
+			_test_ldap_read "$@";;
+		R- )
+			_test_ldap_read "$@" \
+			&& _test_ldap_write -n "$@";;
+		* )
+			_test_ldap_read -n "$@";;
+	esac
+	return $?
+}
+
+# Test modifying an LDAP entry
+function _test_ldap_write() {
+	# Args
+	__test_ldap_parse_args "$@"
+	# Discard args up to and including '--'
+	while [ -n "$1" -a "$1" != "--" ]; do shift; done
+
+	# Save attribute
+	attr_reset=$(ldapsearch -xD "$as" -y "$passwdfile" -b "$to" $attr -LLL \
+		| grep "^$attr")
+	# If save failed
+	if [ $? -ne 0 -o -z "$attr_reset" ]; then
+		echo "!!! ERROR: Can't read attribute '$attr' of '$to' !!!"
+		return 1
+	fi
+
+	# Test write: the new value is the same, but permission is required
+	ldapmodify -xD "$as" -y "$passwdfile" "$@" >/dev/null 2>&1 <<-EOF
+		dn: $to
+		changetype: modify
+		replace: $attr
+		$attr_reset
+	EOF
+	fail=$?
+
+	if [ $fail -ne 0 -a $neg -ne 1 ]; then
+		echo "!!! FAIL: '$as' failed to write attribute '$attr' of '$to' !!!"
+		return 1
+	elif [ $fail -eq 0 -a "$neg" -eq 1 ]; then
+		echo "!!! FAIL: '$as' managed to write attribute '$attr' of '$to' !!!"
+		return 1
+	fi
+}
+
+function _test_ldap_read() {
+	# Args
+	__test_ldap_parse_args "$@"
+	# Discard args up to and including '--'
+	while [ -n "$1" -a "$1" != "--" ]; do shift; done
+
+	# Search for attribute
+	ldapsearch -xD "$as" -y "$passwdfile" -b "$to" $attr "$@" -LLL 2>/dev/null \
+		| grep "^$attr" >/dev/null
+	fail=$?
+
+	if [ $fail -ne 0 -a $neg -ne 1 ]; then
+		echo "!!! FAIL: '$as' failed to read attribute '$attr' of '$to' !!!"
+		return 1
+	elif [ $fail -eq 0 -a $neg -eq 1 ]; then
+		echo "!!! FAIL: '$as' managed to read attribute '$attr' of '$to' !!!"
+		return 1
+	fi
+}
+
+
+function __test_ldap_parse_args() {
+	neg=0
+	as=
+	to=
+	attr=
+
+	while [ -n "$1" ]; do
+		case "$1" in
+			-n | --neg )
+				neg=1
+				shift;;
+			-D | --as )
+				as="$2"
+				shift 2;;
+			-b | --to | --target )
+				to="$2"
+				shift 2;;
+			-a | --attr | --attribute )
+				attr="$2"
+				shift 2;;
+			-- )
+				shift
+				break;;
+			* )
+				echo "Error parsing argument '$1'."
+				echo "Only arguments after '--' are passed down."
+				return 3;
+		esac
+	done
+
+	passwdfile="$PASSWORD_DIR/$as"
+}
