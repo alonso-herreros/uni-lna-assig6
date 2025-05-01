13809c0ef6961acc4f34dc3df6e0d55960e3fb90
Alonso Herreros <alonso.herreros.c@gmail.com>
2025-05-01 18:23:38 +0200

    test:feat(set-passwords): allow setting search dir

--- test/set-passwords.sh
+++ test/set-passwords.sh
@@ -3,6 +3,15 @@
 PASSWORD_DIR="passwords"
 
 ADMIN="cn=admin,dc=marvel,dc=com"
+
+while [ -n "$1" ]; do case "$1" in
+    -p | --passwords )
+        PASSWORD_DIR="$2"
+        shift 2;;
+    * )
+        break;;
+esac; done
+
 PASSWDFILE="$PASSWORD_DIR/$ADMIN"
 
 for newpasswdfile in $PASSWORD_DIR/*,dc=com; do
