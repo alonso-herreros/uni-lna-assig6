f3217fe9ec887e2d95bcfd31c1209dd493c2e0ef
Alonso Herreros <alonso.herreros.c@gmail.com>
2025-05-01 21:09:34 +0200

    feat: Add heroes' general permissions

--- test/test-permissions.sh
+++ test/test-permissions.sh
@@ -191,16 +191,16 @@ test_ldap_access_array -t "Starlord write Title" \
     "$STARLORD" - R- W ? ?  ? ? ?  ? ? ?
 fails=$((fails + $?))
 
-# # By heroes
-# test_ldap_access_array -t "Hero read general" \
-#     "$WOLVERINE" - - - R R  ? ? ?  ? ? ?
-# fails=$((fails + $?))
-# test_ldap_access_array -t "Hero read general" \
-#     "$IRONMAN" - - - R R  ? ? ?  ? ? ?
-# fails=$((fails + $?))
-# test_ldap_access_array -t "Hero read general" \
-#     "$GROOT" - - - R R  ? ? ?  ? ? ?
-# fails=$((fails + $?))
+# By heroes
+test_ldap_access_array -t "Hero read general 1" \
+    "$WOLVERINE" - - - R- R-  ? ? ?  ? ? ?
+fails=$((fails + $?))
+test_ldap_access_array -t "Hero read general 2" \
+    "$IRONMAN" - - - R- R-  ? ? ?  ? ? ?
+fails=$((fails + $?))
+test_ldap_access_array -t "Hero read general 3" \
+    "$GROOT" - - - R- R-  ? ? ?  ? ? ?
+fails=$((fails + $?))
 
 # # Between heroes in the same team, plus their mentors
 # test_ldap_access_array -t "Hero read general" \
